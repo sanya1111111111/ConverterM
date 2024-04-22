cf21d5cb2cb28bff4fd3fee8de0a39729b340d3b
sanya1111111111 <116189775+sanya1111111111@users.noreply.github.com>
2024-04-22 17:19:18 +0300

    Initial commit

---
+++ src/main/java/ImageConverter/FileTools.java
@@ -0,0 +1,20 @@
+package ImageConverter;
+
+import java.io.File;
+
+public class FileTools {
+    private File file;
+    public FileTools(File file){
+        this.file = file;
+    }
+    public String getNameWithoutFormat(){
+        return this.file.getName().substring(0, getDotPosition());
+    }
+    public String getFormat(){
+        return this.file.getName().substring(getDotPosition()+1);
+    }
+    public int getDotPosition(){
+        return this.file.getName().lastIndexOf(".");
+    }
+}
+
---
+++ src/main/java/ImageConverter/fileConverter.java
@@ -0,0 +1,49 @@
+package ImageConverter;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+
+public class fileConverter{
+
+    private final static String[] supportedFormats = {"JPG", "JPEG", "PNG", "BMP", "WBMP","GIF"};
+
+    public static void convert(String inputImagePath, String outputImagePath, String formatName) throws IOException {
+        if (!formatCheck(formatName)) throw new IOException();
+        FileInputStream inputStream = new FileInputStream(inputImagePath);
+        FileOutputStream outputStream = new FileOutputStream(outputImagePath+"."+formatName);
+        BufferedImage inputImage = ImageIO.read(inputStream);
+        ImageIO.write(inputImage, formatName, outputStream);
+        outputStream.close();
+        inputStream.close();
+    }
+    public static String massConvert(String inputDir, String outputDir, String formatName){
+        if (!formatCheck(formatName)) return "Ошибка неверный формат";
+        File dir = new File(inputDir);
+        int sucRate = 0;
+        int errRate = 0;
+        for ( File file : dir.listFiles() ){
+            if ( file.isFile() ) {
+                FileTools managedFile = new FileTools(file);
+                try {
+                    if (formatCheck(managedFile.getFormat())){
+                        fileConverter.convert(file.getAbsolutePath(), outputDir + "\\" + managedFile.getNameWithoutFormat(), formatName);
+                        sucRate += 1;
+                    }
+                    else throw new IOException();
+                } catch (IOException ex) {
+                    errRate += 1;
+                }
+            }
+        }
+        return "Удачно конвертированно: "+sucRate+" Возникла ошибка: "+ errRate;
+    }
+    private static boolean formatCheck(String formatName){
+        return Arrays.asList(supportedFormats).contains(formatName.toUpperCase());
+    }
+}
